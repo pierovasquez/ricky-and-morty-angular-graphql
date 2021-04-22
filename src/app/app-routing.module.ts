ecc04ae363a36c0bb821b711cc3a689f7889891d
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-04-23 01:01:27 +0200

    episodes component created and routes added to the app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'episodes',
+    loadChildren: () =>
+      import('./components/pages/episodes/episodes.module').then(
+        (m) => m.EpisodesModule
+      ),
+  },
+  {
+    path: 'not',
+    loadChildren: () =>
+      import('./components/pages/notFound/not-found.module').then(
+        (m) => m.NotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
