d8585481494a63d0d755a7d64a8809d5174ea65a
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-04-25 23:36:12 +0200

    episodes page finished

--- src/app/components/pages/episodes/episodes.component.ts
+++ src/app/components/pages/episodes/episodes.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Episode } from '@app/shared/interfaces/data.interface';
+import { DataService } from '@app/shared/services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrls: ['./episodes.component.scss']
 })
-export class EpisodesComponent implements OnInit {
+export class EpisodesComponent {
+  episodes$: Observable<Episode[]> = this.dataSvc.episodes$
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    private dataSvc: DataService
+  ) { }
 
 }
