9ea82a44fafe1b4e91aee4f2c7edb95e2e9777b5
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-04-26 22:47:22 +0200

    the initial page which redirects to the application changed to characters-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'character-list',
+    redirectTo: 'characters-list',
     pathMatch: 'full'
   },
   {
