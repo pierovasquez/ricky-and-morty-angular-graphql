512c6d9b3427bebdfbc15311fe38d33efaaa63f2
Piero Vasquez <piero.vasquez.r@gmail.com>
2021-04-26 21:52:37 +0200

    characters-card component modified so it loads the character its get from the input()

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Character } from '@app/shared/interfaces/data.interface';
 
 @Component({
   selector: 'app-characters-card',
   templateUrl: './characters-card.component.html',
-  styleUrls: ['./characters-card.component.scss']
+  styleUrls: ['./characters-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CharactersCardComponent implements OnInit {
+export class CharactersCardComponent {
+
+  @Input() character: Character = <Character>{};
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  onToggleFavorite(): void {
+    this.character.isFavorite = !this.character.isFavorite;
+    this.getIcon();
   }
 
+
+  getIcon(): string {
+    return this.character.isFavorite ? 'heart-solid.svg' : 'heart.svg';
+  }
 }
