32aa6d0a9ea6f49f72f1235f121e6e9033bbeeea
Piero <piero.vasquez.r@gmail.com>
2021-04-28 15:24:41 +0200

    button added to the character-list to get back to the start point of the page

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { DataService } from '@app/shared/services/data.service';
 import { LocalStorageService } from '@app/shared/services/localStorage.service';
 
+const SCROLL_HEIGHT = 500;
 @Component({
   selector: 'app-characters-list',
   templateUrl: './characters-list.component.html',
-  styleUrls: ['./characters-list.component.scss']
+  styleUrls: ['./characters-list.component.scss'],
 })
 export class CharactersListComponent {
   characters$ = this.dataSvc.characters$;
+  showButton = false;
 
   constructor(
     private dataSvc: DataService,
-    private localStorageService: LocalStorageService
-  ) { }
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    console.log('yoff', yOffSet);
+    const scrollTop = this.document.documentElement.scrollTop;
+    console.log('scrolltop', scrollTop);
+    this.showButton = (yOffSet || scrollTop) > SCROLL_HEIGHT;
+  }
+
+  onScrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
 }
